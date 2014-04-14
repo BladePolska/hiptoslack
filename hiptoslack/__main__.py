82676969c470d4e2d303d16401c7ab7bf1cb640d
Marcin Mincer <marcin.mincer@gmail.com>
2014-04-15 01:10:15 +0200

    Initial commit

---
+++ hiptoslack/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""hiptoslack.__main__: executed when bootstrap directory is called as script."""
+
+
+from .hiptoslack import main
+main()
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""setup.py: setuptools control."""
+
+import re
+from setuptools import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('hiptoslack/hiptoslack.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "hiptoslack",
+    packages = ["hiptoslack"],
+    entry_points = {
+        "console_scripts": ['hiptoslack = hiptoslack.hiptoslack:main']
+        },
+    version = version,
+    description = "Python command line application bare bones template.",
+    long_description = long_descr,
+    author = "Marcin Mincer",
+    author_email = "marcin@bladepolska.com",
+    url = "http://www.bladepolska.com",
+    )
